1eed5133867ceb74e211f3342d9e14d3781a7abb
diaaanek <dkorongy@gmail.com>
2019-02-12 17:47:24 -0500

    dragula

--- src/draggable.js
+++ src/draggable.js
@@ -1,49 +1,19 @@
-class App {
-  static init() {
+// dragula library
 
-    App.box = document.getElementsByClassName('box')[0]
 
-    App.box.addEventListener("dragstart", App.dragstart)
-    App.box.addEventListener("dragend", App.dragend)
+dragula([
+	document.getElementById("b1"),
+	document.getElementById("b2")
+])
 
-    const containers = document.getElementsByClassName('holder')
+// // Scrollable area
+// var element = document.getElementById("boards"); // Count Boards
+// var numberOfBoards = element.getElementsByClassName('board').length;
+// var boardsWidth = numberOfBoards*316 // Width of all Boards
+// console.log(boardsWidth);
+// element.style.width = boardsWidth+"px"; // set Width
 
-    for(const container of containers) {
-      container.addEventListener("dragover", App.dragover)
-      container.addEventListener("dragenter", App.dragenter)
-      container.addEventListener("dragleave", App.dragleave)
-      container.addEventListener("drop", App.drop)
-    }
-  }
-
-  static dragstart() {
-    this.className += " held"
-
-    setTimeout(()=>this.className="invisible", 0)
-  }
-
-  static dragend() {
-    this.className = "box"
-  }
-
-  static dragover(e) {
-    e.preventDefault()
-  }
-
-  static dragenter(e) {
-    e.preventDefault()
-    this.className += " hovered"
-  }
-
-  static dragleave() {
-    this.className = "holder"
-  }
-
-  static drop() {
-    this.className = "holder"
-    this.append(App.box)
-  }
-
-}
-
-document.addEventListener("DOMContentLoaded", App.init)
+// disable text-selection
+function disableselect(e) {return false;}
+document.onselectstart = new Function ()
+document.onmousedown = disableselect
