c3d622ca03470a1fdd09a31fa7e4a3b3ba96db2f
diaaanek <dkorongy@gmail.com>
2019-02-12 10:48:31 -0500

    some animations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+
+
+var astro = document.createElement("img")
+astro.setAttribute("src", "assets/astroman.png")
+document.querySelector('#astro').appendChild(astro)
+
+
 function changeText(text)
       {
           var display = document.querySelector('.box');
@@ -12,7 +19,8 @@ function changeText(text)
       }
 
 
-      let img = document.createElement("img");
+
+let img = document.createElement("img");
 img.src = "assets/planet.png";
 var src = document.querySelector('.box');
 src.appendChild(img);
