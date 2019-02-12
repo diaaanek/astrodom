e22ef00eeaf1dd40f5ba2b6c78115adebf349e66
github username <github email address>
2019-02-12 17:42:14 -0500

    booleans displaying on grid

--- src/draggable.js
+++ src/draggable.js
@@ -1,49 +1,49 @@
-class App {
-  static init() {
-
-    App.box = document.getElementsByClassName('box')[0]
-
-    App.box.addEventListener("dragstart", App.dragstart)
-    App.box.addEventListener("dragend", App.dragend)
-
-    const containers = document.getElementsByClassName('holder')
-
-    for(const container of containers) {
-      container.addEventListener("dragover", App.dragover)
-      container.addEventListener("dragenter", App.dragenter)
-      container.addEventListener("dragleave", App.dragleave)
-      container.addEventListener("drop", App.drop)
-    }
-  }
-
-  static dragstart() {
-    this.className += " held"
-
-    setTimeout(()=>this.className="invisible", 0)
-  }
-
-  static dragend() {
-    this.className = "box"
-  }
-
-  static dragover(e) {
-    e.preventDefault()
-  }
-
-  static dragenter(e) {
-    e.preventDefault()
-    this.className += " hovered"
-  }
-
-  static dragleave() {
-    this.className = "holder"
-  }
-
-  static drop() {
-    this.className = "holder"
-    this.append(App.box)
-  }
-
-}
-
-document.addEventListener("DOMContentLoaded", App.init)
+// class App {
+//   static init() {
+//
+//     App.box = document.getElementsByClassName('box')[0]
+//
+//     App.box.addEventListener("dragstart", App.dragstart)
+//     App.box.addEventListener("dragend", App.dragend)
+//
+//     const containers = document.getElementsByClassName('holder')
+//
+//     for(const container of containers) {
+//       container.addEventListener("dragover", App.dragover)
+//       container.addEventListener("dragenter", App.dragenter)
+//       container.addEventListener("dragleave", App.dragleave)
+//       container.addEventListener("drop", App.drop)
+//     }
+//   }
+//
+//   static dragstart() {
+//     this.className += " held"
+//
+//     setTimeout(()=>this.className="invisible", 0)
+//   }
+//
+//   static dragend() {
+//     this.className = "box"
+//   }
+//
+//   static dragover(e) {
+//     e.preventDefault()
+//   }
+//
+//   static dragenter(e) {
+//     e.preventDefault()
+//     this.className += " hovered"
+//   }
+//
+//   static dragleave() {
+//     this.className = "holder"
+//   }
+//
+//   static drop() {
+//     this.className = "holder"
+//     this.append(App.box)
+//   }
+//
+// }
+//
+// document.addEventListener("DOMContentLoaded", App.init)
